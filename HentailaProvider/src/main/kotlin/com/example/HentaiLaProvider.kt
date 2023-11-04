86ac7951eeb4b57abd9e2c6019314df77e4aa80b
RUBIKOF <ca_sim@hotmail.com>
2023-11-04 16:39:10 -0600

    prueba

--- HentailaProvider/src/main/kotlin/com/example/HentaiLaProvider.kt
+++ HentailaProvider/src/main/kotlin/com/example/HentaiLaProvider.kt
@@ -52,9 +52,8 @@ class HentaiLaProvider : MainAPI() {
                 HomePageList(
                         "Últimos episodios",
                         app.get(mainUrl).document.select("#aa-wp > div > section.section.episodes > div > article").map {
-                            val x = it.attr("href").replace("/ver/", "hentai-")
-                            val otro = x.replace("-2","")
-                            val title = otro
+                            val x = it.selectFirst("a")?.attr("href")
+                            val title = x
                             //val title = it.selectFirst("h2")?.text()
                             val dubstat = if (title!!.contains("Latino") || title.contains("Castellano"))
                                 DubStatus.Dubbed else DubStatus.Subbed
