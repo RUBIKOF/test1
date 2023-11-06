76bb5a14c942a261438e383e129df6fa456e3070
RUBIKOF <ca_sim@hotmail.com>
2023-11-06 10:00:36 -0600

    prueba

--- OtroProvider/src/main/kotlin/com/example/OtroProvider.kt
+++ OtroProvider/src/main/kotlin/com/example/OtroProvider.kt
@@ -139,7 +139,8 @@ class OtroProvider : MainAPI() {
         val poster = doc.selectFirst(".set-bg")?.attr("data-setbg")
         val title = doc.selectFirst(".anime__details__title > h3")?.text()
         val type = doc.selectFirst(".anime__details__text")?.text()
-        val description = doc.selectFirst(".anime__details__text > p")?.text()
+        val description = "Numero de capitulos " + doc.select("div.epcontent.col-lg-3.col-md-6.col-sm-6.col-6").size
+        //val description = doc.selectFirst(".anime__details__text > p")?.text()
         val genres = doc.select("div.col-lg-6:nth-child(1) > ul:nth-child(1) > li:nth-child(2) > a")
                 .map { it.text() }
         val status = when (doc.selectFirst("span.enemision")?.text()) {
