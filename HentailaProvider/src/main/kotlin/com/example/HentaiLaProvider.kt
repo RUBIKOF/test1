cfed0a3b73161f61dfbad4b16f4e65992bb6c4c9
RUBIKOF <ca_sim@hotmail.com>
2023-11-09 11:14:19 -0600

    prueba

--- HentailaProvider/src/main/kotlin/com/example/HentaiLaProvider.kt
+++ HentailaProvider/src/main/kotlin/com/example/HentaiLaProvider.kt
@@ -155,7 +155,7 @@ class HentaiLaProvider : MainAPI() {
         val title = doc.selectFirst(".h-title")?.text()
         val type = "Hentai"
         val test1 = app.get("https://www4.hentaila.com/ver/hajimete-no-hitozuma-1", timeout = 120).document
-        val test2 =test1.select("#hentai > script:nth-child(11)")
+        val test2 =test1.select("//*[@id=\"hentai\"]/script[10]")
         val description = "" + test2.text()
         //val description = doc.selectFirst(".h-content > p")?.text()
         val genres = doc.select(".genres > a")
